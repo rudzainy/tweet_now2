6a7752ac0299373fb25a5ea43a39f4e51a30187f
Rudzainy Rahman <rudzainy@gmail.com>
2015-09-09 18:40:33 +0800

    change to ENV

--- config/environment.rb
+++ config/environment.rb
@@ -43,13 +43,13 @@ require 'simple_oauth'
 require 'omniauth-twitter'
 require 'net/https'
 
-TWITTER_API = YAML.load_file('config/secret.yaml')
+# TWITTER_API = YAML.load_file('config/secret.yaml')
 
 $twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = TWITTER_API['API_Key']
-  config.consumer_secret = TWITTER_API['API_Secret']
-  config.access_token = TWITTER_API['Access_Token']
-  config.access_token_secret = TWITTER_API['Access_Token_Secret']
+  config.consumer_key = ENV['API_Key']
+  config.consumer_secret = ENV['API_Secret']
+  config.access_token = ENV['Access_Token']
+  config.access_token_secret = ENV['Access_Token_Secret']
 end
 
 use OmniAuth::Builder do
