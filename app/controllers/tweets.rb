cebc0f28e4f7ead003771a8730b38dbabc7e8b88
Rudzainy Rahman <rudzainy@gmail.com>
2015-09-09 19:47:54 +0800

    changed how session handled

--- app/controllers/tweets.rb
+++ app/controllers/tweets.rb
@@ -1,9 +1,9 @@
 post '/post_tweet' do
 	# byebug
 	tweet = params[:tweet]
-
-	$twitter.access_token = session[:token]
-  $twitter.access_token_secret = session[:token_secret]
+	user = TwitterUser.find_by(name: session[:name])
+	$twitter.access_token = user.token
+  $twitter.access_token_secret = user.token_secret
 	$twitter.update(tweet)
 
 	@flash = "Tweeted!"
--- app/controllers/twitter_users.rb
+++ app/controllers/twitter_users.rb
@@ -5,14 +5,19 @@ end
 get '/auth/twitter/callback' do
   # env['omniauth.auth'] ? session[:admin] = true : halt(401,'Not Authorized')
   
-  session[:handle] = "@" + env['omniauth.auth']['info']['nickname']
+  # session[:handle] = "@" + env['omniauth.auth']['info']['nickname']
   
-  session[:name] = env['omniauth.auth']['info']['name']
-  session[:token] = env['omniauth.auth']['credentials']['token']
-  session[:token_secret] = env['omniauth.auth']['credentials']['secret']
+  # session[:name] = env['omniauth.auth']['info']['name']
+  # session[:token] = env['omniauth.auth']['credentials']['token']
+  # session[:token_secret] = env['omniauth.auth']['credentials']['secret']
 
-  user = TwitterUser.find_or_create_by(handle: session[:handle])
-  user.update(name: session[:name], token: session[:token], token_secret: session[:token_secret]) 
+  user = TwitterUser.find_or_create_by(name:  env['omniauth.auth']['info']['name'])
+  # user.update(name: session[:name], token: session[:token], token_secret: session[:token_secret]) 
+	  user.token = env['omniauth.auth']['credentials']['token']
+	  user.token_secret = env['omniauth.auth']['credentials']['secret']
+  user.save
+  
+  session[:name] = user.name
 
   erb :index
 end
